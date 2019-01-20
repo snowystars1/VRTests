9d7b160c632b66b7637e0d641a44f3ae76cfa1ce
Chris Chaney <32317296+snowystars1@users.noreply.github.com>
2019-01-19 21:28:50 -0500

    Movement Updates
    Controller Movement and Teleport Movement.

--- VR/Assets/OculusInput.cs
+++ VR/Assets/OculusInput.cs
@@ -8,7 +8,9 @@ public class OculusInput : MonoBehaviour
     public GameObject cameraRig;
     public GameObject leftController;
     public GameObject rightController;
+    public Camera mainCam;
 
+    public GameObject teleTarget;
 
     public SteamVR_Action_Single grabGripAction;
     public SteamVR_Action_Single triggerGripAction; 
@@ -27,20 +29,29 @@ public class OculusInput : MonoBehaviour
         //}
 
         gripValue = grabGripAction.GetAxis(SteamVR_Input_Sources.Any);
-
         if (gripValue > 0.2f)
         {
             print(gripValue);
         }
 
-        triggerValue = triggerGripAction.GetAxis(SteamVR_Input_Sources.Any);
-        print(triggerValue);
 
-        if(triggerValue > 0.5f && pressFlag)//Teleport using raycast
+        //print(triggerValue);
+        triggerValue = triggerGripAction.GetAxis(SteamVR_Input_Sources.Any);
+        if(triggerValue > 0.1f)
+        {
+            int layerMask = (1 << 9);
+            if (Physics.Raycast(rightController.transform.position, rightController.transform.forward - rightController.transform.up, out RaycastHit hit, 50f, layerMask))
+            {
+                teleTarget.transform.position = hit.point;
+                Debug.DrawRay(rightController.transform.position, (rightController.transform.forward - rightController.transform.up) * 100f, Color.blue,5f);
+            }
+        }
+
+        if(triggerValue > 0.75f && pressFlag)//Teleport using raycast
         {
             print("Teleport");
-            RaycastHit hit;
-            if(Physics.Raycast(rightController.transform.position, rightController.transform.forward, out hit, 100f))
+            Debug.DrawRay(rightController.transform.position, rightController.transform.forward * 100f, Color.red);
+            if (Physics.Raycast(rightController.transform.position, rightController.transform.forward - rightController.transform.up, out RaycastHit hit, 100f))
             {
                 cameraRig.transform.position = hit.point;
 
@@ -50,16 +61,20 @@ public class OculusInput : MonoBehaviour
         if (triggerValue < 0.1f)//Must release or almost release trigger before teleporting again
             pressFlag = true;
 
-        moveValue = joyStickAction.GetAxis(SteamVR_Input_Sources.Any);
 
+        moveValue = joyStickAction.GetAxis(SteamVR_Input_Sources.Any);
         if (moveValue.y > 0.1f || moveValue.y < -0.1f)
         {//Forward and Backward
-            cameraRig.transform.Translate(Vector3.forward * moveValue.y);
-        }
+            Vector3 controllerMove = new Vector3(((rightController.transform.forward - rightController.transform.up).normalized).x,
+                0f,
+                (((rightController.transform.forward - rightController.transform.up).normalized).z));
 
+            cameraRig.transform.Translate(controllerMove*moveValue.y);
+        }
         if(moveValue.x > 0.1f || moveValue.x < -0.1f)
         {//Right and Left
-            cameraRig.transform.Translate(Vector3.right * moveValue);
+            Vector3 controllerMove = new Vector3(rightController.transform.right.x, 0f, rightController.transform.right.z);
+            cameraRig.transform.Translate(controllerMove * moveValue.x);
         }
 
         //Vector2 joyStickValue = joyStickAction.GetAxis(SteamVR_Input_Sources.Any);
