df886af959f9954e6935c1e9d4d549a5aaa4c0a6
Chris Chaney <32317296+snowystars1@users.noreply.github.com>
2019-02-16 23:29:38 -0500

    Learning Procedural Mesh Creation and Creating Basic Triangles
    Heres the strat:
    Because meshes are always created in local space and there is no way to move the object that has a mash without moving the mesh as well, I will implement a non-moving gameobject that gets created when we start our slash. THEN, the gameobject will probably start moving in a direction (The direction the player was looking when they started the swing?) However, the mesh will still be created at our paintbrush, while the object is moving forward, therefore it will give the illusion that the slash is moving forward and we are swinging it. The next step is to figure out the local space/world space transforming I'm going to have to do to get the mesh to be parented to a stable object, but be created at the tip of my paint brush.  Then inputs and actual functionality and stuff.

---
+++ VR/Assets/BasicMeshCreationTest.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMeshCreationTest : MonoBehaviour
+{
+    private Mesh triangleMesh;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        triangleMesh = GetComponent<MeshFilter>().mesh;
+    }
+
+    public void SpawnTriangle(Vector3 position)
+    {
+        triangleMesh.Clear();
+        print("hi");
+        Vector3[] vertices = new Vector3[3];
+        int[] triangles = new int[3];
+        Vector3[] normals = new Vector3[3];
+
+        vertices[0] = transform.InverseTransformPoint(transform.position);
+        vertices[1] = transform.InverseTransformPoint(transform.position + new Vector3(1,0,0));
+        vertices[2] = transform.InverseTransformPoint(transform.position + new Vector3(0, 0, 1));
+
+        normals[0] = Vector3.up;
+        normals[1] = Vector3.up;
+        normals[2] = Vector3.up;
+
+        triangles[0] = 2;
+        triangles[1] = 1;
+        triangles[2] = 0;
+
+        triangleMesh.vertices = vertices;
+        triangleMesh.triangles = triangles;
+        triangleMesh.normals = normals;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SpawnTriangle(transform.position);
+        }
+    }
+}
---
+++ VR/Assets/MeshLineRenderer.cs
@@ -0,0 +1,176 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+ 
+/*
+class Point {
+    public Vector3 p;
+    public Point next;
+}
+*/
+ 
+[RequireComponent(typeof(MeshRenderer))]
+[RequireComponent(typeof(MeshFilter))]
+public class MeshLineRenderer : MonoBehaviour {
+ 
+    public Material lineMaterial;
+ 
+    private Mesh lineMesh;
+ 
+    private Vector3 startPoint;
+ 
+    private float lineSize = .1f;
+ 
+    private bool firstQuad = true;
+ 
+    void Start() {
+        lineMesh = GetComponent<MeshFilter>().mesh;
+        GetComponent<MeshRenderer>().material = lineMaterial;
+    }
+ 
+    public void setWidth(float width) {
+        lineSize = width;
+    }
+ 
+    public void AddPoint(Vector3 point) {
+        if(startPoint != Vector3.zero) {
+            AddLine(lineMesh, MakeQuad(startPoint, point, lineSize, firstQuad));
+            firstQuad = false;
+        }
+ 
+        startPoint = point;
+    }
+ 
+    Vector3[] MakeQuad(Vector3 start, Vector3 end, float width, bool firstQuadorNot) {
+        width = width / 2;
+ 
+        Vector3[] quadCreation;
+        if(firstQuadorNot) {
+            quadCreation = new Vector3[4];
+        } else {
+            quadCreation = new Vector3[2];
+        }
+ 
+        Vector3 n = Vector3.Cross(start, end);
+        Vector3 l = Vector3.Cross(n, end - start);
+        l.Normalize();
+ 
+        if(firstQuadorNot) {
+            quadCreation[0] = transform.InverseTransformPoint(start + l * width);
+            quadCreation[1] = transform.InverseTransformPoint(start + l * -width);
+            quadCreation[2] = transform.InverseTransformPoint(end + l * width);
+            quadCreation[3] = transform.InverseTransformPoint(end + l * -width);
+        } else {
+            quadCreation[0] = transform.InverseTransformPoint(start + l * width);
+            quadCreation[1] = transform.InverseTransformPoint(start + l * -width);
+        }
+        return quadCreation;
+    }
+ 
+    void AddLine(Mesh m, Vector3[] quad) {
+        int lineVertexCount = m.vertices.Length;
+ 
+        Vector3[] vertices = m.vertices;
+        vertices = resizeVertices(vertices, 2 * quad.Length);
+ 
+        //Assign new vertices
+        for(int i = 0; i < 2*quad.Length; i += 2) {
+            vertices[lineVertexCount + i] = quad[i / 2];
+            vertices[lineVertexCount + i + 1] = quad[i / 2];
+        }
+ 
+        Vector2[] uvs = m.uv;
+        uvs = resizeUVs(uvs, 2 * quad.Length);
+ 
+        if(quad.Length == 4) {
+            uvs[lineVertexCount] = Vector2.zero;
+            uvs[lineVertexCount + 1] = Vector2.zero;
+            uvs[lineVertexCount + 2] = Vector2.right;
+            uvs[lineVertexCount + 3] = Vector2.right;
+            uvs[lineVertexCount + 4] = Vector2.up;
+            uvs[lineVertexCount + 5] = Vector2.up;
+            uvs[lineVertexCount + 6] = Vector2.one;
+            uvs[lineVertexCount + 7] = Vector2.one;
+        } else {
+            if(lineVertexCount % 8 == 0) {
+                uvs[lineVertexCount] = Vector2.zero;
+                uvs[lineVertexCount + 1] = Vector2.zero;
+                uvs[lineVertexCount + 2] = Vector2.right;
+                uvs[lineVertexCount + 3] = Vector2.right;
+ 
+            } else {
+                uvs[lineVertexCount] = Vector2.up;
+                uvs[lineVertexCount + 1] = Vector2.up;
+                uvs[lineVertexCount + 2] = Vector2.one;
+                uvs[lineVertexCount + 3] = Vector2.one;
+            }
+        }
+ 
+        int trianglesLength = m.triangles.Length;
+ 
+        int[] triangles = m.triangles;
+        triangles = resizeTriangles(triangles, 12);
+ 
+        if(quad.Length == 2) {
+            lineVertexCount -= 4;
+        }
+ 
+        // front-facing quad
+        triangles[trianglesLength] = lineVertexCount;
+        triangles[trianglesLength + 1] = lineVertexCount + 2;
+        triangles[trianglesLength + 2] = lineVertexCount + 4;
+ 
+        triangles[trianglesLength + 3] = lineVertexCount + 2;
+        triangles[trianglesLength + 4] = lineVertexCount + 6;
+        triangles[trianglesLength + 5] = lineVertexCount + 4;
+ 
+        // back-facing quad
+        triangles[trianglesLength + 6] = lineVertexCount + 5;
+        triangles[trianglesLength + 7] = lineVertexCount + 3;
+        triangles[trianglesLength + 8] = lineVertexCount + 1;
+ 
+        triangles[trianglesLength + 9] = lineVertexCount + 5;
+        triangles[trianglesLength + 10] = lineVertexCount + 7;
+        triangles[trianglesLength + 11] = lineVertexCount + 3;
+ 
+        m.vertices = vertices;
+        m.uv = uvs;
+        m.triangles = triangles;
+        m.RecalculateBounds();
+        m.RecalculateNormals();
+    }
+ 
+    Vector3[] resizeVertices(Vector3[] oldVertices, int newSize) {
+        Vector3[] newVertices = new Vector3[oldVertices.Length + newSize];
+        for(int i = 0; i < oldVertices.Length; i++) {
+            newVertices[i] = oldVertices[i];
+        }
+ 
+        return newVertices;
+    }
+ 
+    Vector2[] resizeUVs(Vector2[] oldUVs, int newSize) {
+        Vector2[] nvs = new Vector2[oldUVs.Length + newSize];
+        for(int i = 0; i < oldUVs.Length; i++) {
+            nvs[i] = oldUVs[i];
+        }
+ 
+        return nvs;
+    }
+ 
+    int[] resizeTriangles(int[] ovs, int ns) {
+        int[] nvs = new int[ovs.Length + ns];
+        for(int i = 0; i < ovs.Length; i++) {
+            nvs[i] = ovs[i];
+        }
+ 
+        return nvs;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            AddPoint(transform.position);
+        }
+    }
+}
